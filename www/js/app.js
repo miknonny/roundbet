a175240de2de7f00d68dfad3a33e4567826d0334
nonnycode <miknonny@gmail.com>
2014-08-13 11:54:54 +0100

    changed double colon to single colons for template urls

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
@@ -32,9 +32,9 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
     // setup an abstract state for the tabs directive
     .state('tab', {
-      url: "/tab",
+      url: '/tab',
       abstract: true,
-      templateUrl: "templates/tabs.html"
+      templateUrl: 'templates/tabs.html'
     })
 
     // Each tab has its own nav history stack:
