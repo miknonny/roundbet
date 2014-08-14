b2a9ae71e395a9b4242e1c8769e3c7c7d2a4ae79
nonnycode <miknonny@gmail.com>
2014-08-14 22:04:45 +0100

    added states for home, login and signup in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -22,19 +22,32 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
+
 .config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      abstract: true,
+      templateUrl: 'templates/home.html'
+    })
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+    .state('home.login', {
+      url:'/login',
+      templateUrl: 'login.html',
+      controller: 'LoginCtrl'
+    })
+
+    .state('home.signup', {
+      url:'/signup',
+      templateUrl: 'signup.html',
+      controller: 'SignupCtrl'
+    });
   
-
-    // setup an abstract state for the tabs directive
-    
-
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/home');
-
+  // .state('', {
+  //     url:'',
+  //     templateUrl: '',
+  //     controller: ''
+  //   });
 });
 
