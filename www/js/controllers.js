4f2e1d4bc3507e7bfadcf2fd26fab6e76f9bec6b
nonnycode <miknonny@gmail.com>
2014-08-14 21:39:53 +0100

    deleted most code and left a blank template

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,16 +2,5 @@
 
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('noController', function() {
 });
